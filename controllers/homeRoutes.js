3fc898382aa1ffb115579b217c013a7c654af636
Nick Graffis <nicholasgraffis@gmail.com>
2020-12-19 13:28:15 -0800

    removed get route that creates portal, run /api/portals to create a portal instead with POST

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -85,23 +85,4 @@ router.get('/liarliar/lobby/:code', async (req, res) => {
   }
 })
 
-router.get('/liarliar/lobby', async (req, res) => {
-  try {
-    console.log(res)
-    const portal = await Portal.create({code: "manly-gorilla12" , game: "liarliar"}, {
-      include: [
-        { model: Round },
-        { model: User }
-      ],
-      attributes: ['id', 'code', 'round'],
-    })
-
-    res.render("lobby", {portal})
-  } catch (err) {
-
-    console.log(err)
-    res.status(500).json(err)
-
-  }
-})
 module.exports = router
