60bd98a226aad60cfcd2ada6d21a99d9d1d9ab7b
Steve Tong <stevetong53@yahoo.com>
2020-12-27 19:32:36 -0800

    travis format to push to main

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,20 @@
-const sequelize = require('../config/connection')
-const { Question, Portal } = require('../models')
-const questionData = require('./questions.json')
-const { Op } = require("sequelize");
+// const sequelize = require('../config/connection');
+const { Question } = require('../models');
+const questionData = require('./questions.json');
+// const { Op } = require('sequelize');
 
 const questions = async () => {
   try {
+    await Question.destroy({ where: {}, force: true });
 
-    await Question.destroy({where: { }, force: true})
+    await Question.bulkCreate(questionData.normal, {
+      ignoreDuplicates: true,
+    });
 
-    const seededQuestions = await Question.bulkCreate(questionData.normal, {
-      ignoreDuplicates: true
-    })
-
-    process.exit(0)
-
+    process.exit(0);
   } catch (err) {
-
-    console.log(err)
-
+    console.log(err);
   }
-}
+};
 
-questions()
+questions();
