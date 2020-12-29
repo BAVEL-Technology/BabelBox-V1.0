5cde90e1c56a4be70f1acbc88437f6502742af79
Robert Schmahl <rschm007@gmail.com>
2020-12-28 19:03:40 -0800

    added cdns for barba and gsap

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,3 @@
-// ====================================================
-// DEPENDENCIES
-// ====================================================
-// start Barba
-Barba.Pjax.start();
-
 /*
  * Parse DOM for twemojis
  * use npm twemoji to convert from emoji to twemoij
@@ -11,25 +5,64 @@ Barba.Pjax.start();
 twemoji.parse(document.body);
 
 // ====================================================
-// WORKING JS
+// GSAP AND BARBA PAGE TRANSITION ANIMATION PROPERTIES
 // ====================================================
 $(document).ready(function () {
   // GSAP Timeline
   const pageTransition = () => {
     let timeline = gsap.timeline();
     // this is where the 5 list items come up on the page during the animation
-    timeline.to('ul.transition li', {
+    timeline.to("ul.transition li", {
       duration: 0.5,
       scaleY: 1,
-      transformOrigin: 'bottom left',
+      transformOrigin: "bottom left",
       stagger: 0.2,
     });
-    timeline.to('ul.transition li', {
+    timeline.to("ul.transition li", {
       duration: 0.5,
       scaleY: 0,
-      transformOrigin: 'bottom left',
+      transformOrigin: "bottom left",
       stagger: 0.1,
       delay: 0.2,
     });
   };
+
+  // define gsap contentAnimation
+  // this animation will cause DOM objects to fade in from an opacity of 0
+  function contentAnimation() {
+    let timeline = gsap.timeline();
+    timeline.from(".wrapper", { duration: 1, opacity: 0, delay: 0.1 });
+  }
+
+  // set delay function
+  function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+      setTimeout(() => {
+        done();
+      }, n);
+    });
+  }
+
+  // barba init
+  barba.init({
+    sync: true,
+    transitions: [
+      {
+        async leave(data) {
+          const done = this.async();
+
+          pageTransition();
+          await delay(1500);
+          done();
+        },
+        async enter(data) {
+          contentAnimation();
+        },
+        async once(data) {
+          contentAnimation();
+        },
+      },
+    ],
+  });
 });
