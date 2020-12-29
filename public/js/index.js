6211309f814ff490e01b33906fb52d8acf045846
Robert Schmahl <rschm007@gmail.com>
2020-12-28 19:33:28 -0800

    settled on basic barba animation for page transitions

--- public/js/index.js
+++ public/js/index.js
@@ -8,43 +8,7 @@ twemoji.parse(document.body);
 // GSAP AND BARBA PAGE TRANSITION ANIMATION PROPERTIES
 // ====================================================
 $(document).ready(function () {
-  // // GSAP Timeline
-  // const pageTransition = () => {
-  //   let timeline = gsap.timeline();
-  //   // this is where the 3 list items come up on the page during the animation
-  //   timeline.to("ul.transition li", {
-  //     duration: 0.5,
-  //     scaleY: 1,
-  //     transformOrigin: "bottom left",
-  //     stagger: 0.2,
-  //   });
-  //   timeline.to("ul.transition li", {
-  //     duration: 0.5,
-  //     scaleY: 0,
-  //     transformOrigin: "bottom left",
-  //     stagger: 0.1,
-  //     delay: 0.2,
-  //   });
-  // };
-
-  // // define gsap contentAnimation
-  // // this animation will cause DOM objects to fade in from an opacity of 0
-  // const contentAnimation = () => {
-  //   let timeline = gsap.timeline();
-  //   timeline.from(".wrapper", { duration: 1, opacity: 0, delay: 0.1 });
-  // };
-
-  // // set delay function
-  // const delay = (n) => {
-  //   n = n || 2000;
-  //   return new Promise((done) => {
-  //     setTimeout(() => {
-  //       done();
-  //     }, n);
-  //   });
-  // };
-
-  // barba init
+  // basic barba/gsap page transition animation
   barba.init({
     sync: true,
     transitions: [
