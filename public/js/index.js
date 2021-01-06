66af6ed4572af1a1c207e1d684cea06550c947e4
Steve Tong <stevetong53@yahoo.com>
2021-01-05 20:39:26 -0800

    small bug fix

--- public/js/index.js
+++ public/js/index.js
@@ -6,25 +6,25 @@ twemoji.parse(document.body);
 
 // ====================================================
 // GSAP AND BARBA PAGE TRANSITION ANIMATION PROPERTIES
-// ====================================================
-$(document).ready(function () {
-  // basic barba/gsap page transition animation
-  barba.init({
-    sync: true,
-    transitions: [
-      {
-        name: "opacity-transition",
-        leave(data) {
-          return gsap.to(data.current.container, {
-            opacity: 0,
-          });
-        },
-        enter(data) {
-          return gsap.from(data.next.container, {
-            opacity: 0,
-          });
-        },
-      },
-    ],
-  });
-});
+// // ====================================================
+// $(document).ready(function () {
+//   // basic barba/gsap page transition animation
+//   barba.init({
+//     sync: true,
+//     transitions: [
+//       {
+//         name: "opacity-transition",
+//         leave(data) {
+//           return gsap.to(data.current.container, {
+//             opacity: 0,
+//           });
+//         },
+//         enter(data) {
+//           return gsap.from(data.next.container, {
+//             opacity: 0,
+//           });
+//         },
+//       },
+//     ],
+//   });
+// });
